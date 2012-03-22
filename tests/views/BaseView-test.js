cb59eb3da9b822db1fb902c90a8bcd089f7f8b97
Scott van Looy <scott@quul.net>
2012-03-22 09:11:20 +0100

    Moved tests around

---
+++ tests/views/BaseView-test.js
@@ -0,0 +1,10 @@
+/*jslint bitwise: false, browser: true, windows: false, evil: false, white: false, plusplus: true, indent: 4 */
+/*globals FF:false,TestCase:false,assertEquals:false,expectAsserts:false,assertFunction:false,assertNoException:false*/
+TestCase("Test the BaseView object", {
+	testPut : function () {
+		expectAsserts(0);
+	},
+	testClear : function () {
+		expectAsserts(0);
+	}
+});
