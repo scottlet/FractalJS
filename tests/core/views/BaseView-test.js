3d9ab66a6ba0e1fe589d516f11000aa477f8dba4
Scott van Looy <scott@quul.net>
2012-04-10 08:21:42 +0200

    View tests

--- tests/core/views/BaseView-test.js
+++ tests/core/views/BaseView-test.js
@@ -18,21 +18,28 @@ TestCase("Test the BaseView object", {
 	tearDown : function () {
 		test.TestView = null;
 	},
-	"test creation of view" : function () {
-		expectAsserts(2);
+	"test createView" : function () {
+		expectAsserts(1);
 		
 		
 		assertNoException(function () {
 			FF.core.views.BaseView.createView(test.TestView);
 		});
+	},
+	"test view can be instantiated" : function () {
+		expectAsserts(1);
 		test.TestView = new test.TestView();
 		assertObject(test.TestView);
 	},
 	"test BaseView API methods exist on new child View" : function () {
+		expectAsserts(6);
 		FF.core.views.BaseView.createView(test.TestView);
 		test.TestView = new test.TestView();
 		assertFunction(test.TestView.setDefaultComponents);
 		assertFunction(test.TestView.requires);
+		assertFunction(test.TestView.createView);
+		assertNotUndefined(test.TestView.uis);
+		assertNotUndefined(test.TestView.controller);
 		assertEquals(test.TestView.enter(), "example text 2");
 	}
 });
